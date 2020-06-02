47e2ee797050631b4de9cac4974babc32abac601
BadWolf <artyom.muraweow2018@yandex.ru>
2020-06-02 20:55:48 +0400

    fix

--- scripts/searchProject.js
+++ scripts/searchProject.js
@@ -8,15 +8,21 @@ let requestSearch;
 search.addEventListener("input",function (event) {
     event.preventDefault();
     requestSearch = this.value;
-    searchInList(requestSearch,listNameProject)
+    let hasElement = searchInList(requestSearch,listNameProject);
+    if (!hasElement){
+        document.getElementById('no-found').style.display = 'block';
+    } else document.getElementById('no-found').style.display = 'none';
 })
 searchInList = function (requestSearch, list){
+    let hasElement = false;
     for (let i = 0; i < list.length; i++){
         console.log(list[i].textContent)
         if (list[i].textContent.toLowerCase().indexOf(requestSearch.toLowerCase()) === -1){
             list[i].parentElement.parentElement.style.display = 'none';
         } else {
+            hasElement = true;
             list[i].parentElement.parentElement.style.display = 'flex';
         }
     }
+    return hasElement;
 }
